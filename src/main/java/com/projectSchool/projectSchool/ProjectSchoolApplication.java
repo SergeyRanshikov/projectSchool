ffc3e7e5ef89bd44c6f74e38f077ccc233fc968f
SergeyRanshikov <ranss@rambler.ru>
2023-09-16 14:42:27 +0300

    init commit

---
+++ src/main/java/com/projectSchool/projectSchool/ProjectSchoolApplication.java
@@ -0,0 +1,14 @@
+package com.projectSchool.projectSchool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSchoolApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSchoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projectSchool/projectSchool/ProjectSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectSchool.projectSchool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
